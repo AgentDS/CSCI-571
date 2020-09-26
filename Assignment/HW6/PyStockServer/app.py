f479d13cc073248a80c259fa1cc5de64817d4c7a
AgentDS <zszxlsq@gmail.com>
2020-09-26 22:05:00 +0800

    use send static file to load index page

--- Assignment/HW6/PyStockServer/app.py
+++ Assignment/HW6/PyStockServer/app.py
@@ -5,15 +5,22 @@
 # @File    : app.py
 # @Software: PyCharm
 from flask import Flask, jsonify, render_template, abort, request
+import os
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder='static')
 
 
 @app.route('/')
-@app.route('/search/', methods=['POST'])
+@app.route('/index')
 def index():
-    ticker_name = request.form['ticker_name']
-    return ('The entered ticker is %s' % ticker_name)
+    return app.send_static_file('hw6_StockSearch.html')
+
+@app.route('/api/v1.0/search')
+def search_begin():
+    return 'The entered ticker is %s' % 'dgafdg'  # ticker_name
+
+# ticker_name = request.form["ticker_name"]
 
 
 if __name__ == '__main__':
--- Assignment/HW6/PyStockServer/static/script.js
+++ Assignment/HW6/PyStockServer/static/script.js
@@ -8,11 +8,30 @@ function obtain_stock_name(event) {
     let tickerNameLen = tickerName.length;
     if (tickerNameLen >= 1) {
         event.preventDefault();
-        console.log('The entered ticker name is:' + tickerName);
-    } else {
-        event.preventDefault();
-        alert('prevent is checked!')
+        var xhr = new XMLHttpRequest();
+        var ll = "/api/v1.0/search"
+        xhr.open("GET", ll)
+        console.log("URL: " + ll)
+        xhr.send("ticker_name=" + tickerName);
+        console.log("XMLHTTPRequest is sent: \'ticker_name=" + tickerName + "\'");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                console.log("HTTP request from script finished");
+                if (xhr.status === 200) {
+                    console.log("status=200, response text: " + xhr.responseText);
+                } else {
+                    console.log("status != 200")
+                    console.error(xhr.statusText);
+                }
+            }
+        }
+
+
     }
+    // else {
+    //     event.preventDefault();
+    //     alert('prevent is checked!')
+    // }
 }
 
 SubmitButton.addEventListener("click", obtain_stock_name, false)
