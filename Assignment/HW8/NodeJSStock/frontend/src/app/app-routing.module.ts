0447a3ae22e0ddbd016fd1ed8ccb58e27aa7fb26
AgentDS <zszxlsq@gmail.com>
2020-10-27 19:39:03 +0800

    add router for major component

--- Assignment/HW8/NodeJSStock/frontend/src/app/app-routing.module.ts
+++ Assignment/HW8/NodeJSStock/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsComponent } from "./details/details.component";
+import { WatchlistComponent } from "./watchlist/watchlist.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { SearchFormComponent } from "./search-form/search-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SearchFormComponent },
+  { path: 'details/:ticker', component: DetailsComponent },
+  { path: 'watchlist', component: WatchlistComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
