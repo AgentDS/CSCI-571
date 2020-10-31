f3b6013946fa4d156f2e316e32d1ae4b834d711c
AgentDS <zszxlsq@gmail.com>
2020-10-31 23:02:32 +0800

    finish news detail modal social share funcitons

--- Assignment/HW8/NodeJSStock/frontend/src/app/news-detail/news-detail.component.ts
+++ Assignment/HW8/NodeJSStock/frontend/src/app/news-detail/news-detail.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { HttpParameterCodec } from '@angular/common/http';
 import { News } from '../news';
 
 @Component({
   selector: 'app-news-detail',
   templateUrl: './news-detail.component.html',
-  styleUrls: ['./news-detail.component.css']
+  styleUrls: ['./news-detail.component.css'],
 })
 export class NewsDetailComponent implements OnInit {
   @Input() public news: News;
+  fbSrc;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    console.log(this.news);
+    this.fbSrc =
+      'https://www.facebook.com/sharer/sharer.php?u=' +
+      encodeURIComponent(this.news.url) +
+      '&amp;src=sdkpreparse';
   }
 
+  goToLink(url: string) {
+    window.open(url, '_blank');
+  }
 }
-
