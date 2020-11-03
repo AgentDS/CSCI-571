09719a6c63cae45d024dee232e942495afb5f41a
AgentDS <zszxlsq@gmail.com>
2020-11-04 02:10:45 +0800

    modify log msg format

--- Assignment/HW8/NodeJSStock/backend-old/server.js
+++ Assignment/HW8/NodeJSStock/backend-old/server.js
@@ -11,11 +11,11 @@ function ignoreFavicon(req, res, next) {
     next();
 }
 
-function checkArray(priceRes) {
+function checkArray(priceRes, tickerName) {
     if (Array.isArray(priceRes)) {
-        console.log('Record length: ' + priceRes.length);
+        console.log(`${tickerName} Record length: ${priceRes.length}`);
     } else {
-        console.log("Ticker Not Found");
+        console.log(`${tickerName} Ticker Not Found`);
     }
 }
 
@@ -28,12 +28,11 @@ app.get('/', (req, res) => {
 
 
 app.get('/api/v1.0.0/searchutil/:keyword', async function (req, res) {
-    console.log("\nSearch-utilities Call");
-    console.log("Keyword: " + req.params.keyword);
+    console.log(`\nSearch-utilities Call: ${req.params.keyword}`);
     // if not found, response is [] with length 0
     let origRes = await outerAPI.getAutocomplete(req.params.keyword);
-    console.log("Length of response: " + origRes.length + "\n");
-    console.log("Search-utilities finished at " + Date());
+    let msg = `${req.params.keyword} Search-utilities finished at ${Date()}\nLength of response: ${origRes.length}`;
+    console.log(msg);
     return res.send(origRes);
     // if (origRes)
     //     return res.status(200).json(origRes);
@@ -44,7 +43,7 @@ app.get('/api/v1.0.0/metadata/:tickerName', async function (req, res) {
     console.log("Ticker: " + req.params.tickerName.toUpperCase());
     // if not found, response is {"detail":"Not found."}
     let origRes = await outerAPI.getCompanyMetaData(req.params.tickerName);
-    console.log("Company Meta Data finished at " + Date());
+    console.log(req.params.tickerName.toUpperCase() + " Meta Data finished at " + Date());
     return res.send(origRes);
     // if (origRes)
     //     return res.status(200).json(origRes);
@@ -56,52 +55,45 @@ app.get('/api/v1.0.0/latestprice/:tickerName', async function (req, res) {
     console.log("Ticker: " + req.params.tickerName.toUpperCase());
     // if not found, response is {"detail":"Not found."}
     let origRes = await outerAPI.getLatestPrice(req.params.tickerName);
-    console.log("Company Latest Price finished at " + Date());
+    console.log(req.params.tickerName.toUpperCase() + " Latest Price finished at " + Date());
     return res.send(origRes);
 })
 
 
 app.get('/api/v1.0.0/news/:keyword', async function (req, res) {
-    console.log("\nNews Call");
-    console.log("Keyword: " + req.params.keyword);
+    console.log(`\nNews Call: ${req.params.keyword.toUpperCase()}`);
     // if error in fetch, response is null
-
     let origRes = await outerAPI.getNews(req.params.keyword);
-
+    let msg = req.params.keyword.toUpperCase() + " News finished at " + Date();
     if (origRes && origRes.length) {
-        console.log("Length of response: " + origRes.length);
-        console.log("sample news title length:" + origRes[0].title.length);
+        msg += "Length of response: " + origRes.length + "; First title length: " + origRes[0].title.length + "\n";
     } else {
-        console.log("Null news.");
+        msg += "Null news.\n";
     }
-    console.log("News finished at " + Date());
+    console.log(msg);
     return res.send(origRes);
     // if (origRes)
     //     return res.status(200).json(origRes);
 })
 
 app.get('/api/v1.0.0/dailycharts/:tickerName/date/:startDate', async function (req, res) {
-    console.log("\nCompany Last day’s chart data Call");
-    console.log("Ticker: " + req.params.tickerName.toUpperCase());
-    console.log("Start Date: " + req.params.startDate);
+    console.log(`\nDaily chart data Call: ${req.params.tickerName.toUpperCase()}; Start Date: ${req.params.startDate}`);
     // if not found, response is {"detail":"Not found."}
     let origRes = await outerAPI.getDailyChartData(req.params.startDate, req.params.tickerName);
-    checkArray(origRes);
-    console.log("Company Last day’s chart data finished at " + Date());
+    checkArray(origRes, req.params.tickerName.toUpperCase());
+    console.log(`${req.params.tickerName.toUpperCase()} daily chart data finished at ${Date()}`);
     return res.send(origRes);
     // if (origRes)
     //     return res.status(200).json(origRes);
 })
 
 app.get('/api/v1.0.0/histcharts/:tickerName/date/:startDate', async function (req, res) {
-    console.log("\nCompany Historical chart data Call");
-    console.log("Ticker: " + req.params.tickerName.toUpperCase());
-    console.log("Start Date: " + req.params.startDate);
+    console.log(`Historical chart data Call: ${req.params.tickerName.toUpperCase()}; Start Date: ${req.params.startDate}\n`);
     // if not found, response is object {"detail":"Error: Ticker 'xxxx' not found"}
     // otherwise response is array of object
     let origRes = await outerAPI.getHistChartsData(req.params.startDate, req.params.tickerName);
-    checkArray(origRes);
-    console.log("Company Historical chart data finished at " + Date());
+    checkArray(origRes, req.params.tickerName.toUpperCase());
+    console.log(`${req.params.tickerName.toUpperCase()} Historical chart data finished at ${Date()}\n`);
     return res.send(origRes);
     // if (origRes)
     //     return res.status(200).json(origRes);
