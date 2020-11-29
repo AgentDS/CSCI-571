9a0812d91be26303b26b70ef955b8ebea960b80e
AgentDS <zszxlsq@gmail.com>
2020-11-30 03:56:49 +0800

    delete default toolbar

--- Assignment/HW9/StockApp/app/src/main/java/com/example/stockapp/MainActivity.java
+++ Assignment/HW9/StockApp/app/src/main/java/com/example/stockapp/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        setTheme(R.style.Theme_StockApp);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
