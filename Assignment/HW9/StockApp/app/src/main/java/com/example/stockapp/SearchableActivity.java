ebfd0b560c719d8c23845362901c6b658d606285
AgentDS <zszxlsq@gmail.com>
2020-12-01 12:46:38 +0800

    detail page tollbar add, no action

--- Assignment/HW9/StockApp/app/src/main/java/com/example/stockapp/SearchableActivity.java
+++ Assignment/HW9/StockApp/app/src/main/java/com/example/stockapp/SearchableActivity.java
@@ -1,11 +1,21 @@
 package com.example.stockapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SearchableActivity extends AppCompatActivity {
 
@@ -15,9 +25,12 @@ public class SearchableActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searchable_layout);
+        // set toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        // Get the intent, verify the action and get the query
-        handleIntent(getIntent());
+        // Get the intent, verify the action and get the query, handled in onNewIntent
+        // handleIntent(getIntent());
     }
 
     @Override
@@ -33,5 +46,27 @@ public class SearchableActivity extends AppCompatActivity {
             Log.i(TAG, "handleIntent: query=" + query);
 //            doMySearch(query);
         }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.detail_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.back:
+                Toast.makeText(this, "Back button clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.star:
+                Toast.makeText(this,"Star clicked", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
